700e9b66f1a2b07f2f7a65b4ab8e583a1ccf07d5
Emad Baqeri <ebaqeri@protonmail.com>
2023-01-12 19:58:21 +0330

    updated styling issues

--- styled/landing.styled.ts
+++ styled/landing.styled.ts
@@ -3,17 +3,20 @@ import styled from "styled-components";
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
-  padding-top: 2rem;
   align-items: center;
   justify-content: space-around;
   height: 100vh;
   width: 100%;
   background-color: #6244ec;
+  position: relative;
 `;
 
 export const IconsContainer = styled.div`
   display: flex;
   flex: row;
+  position: absolute;
+  bottom: 2rem;
+  background-color: none;
 `;
 
 export const IconContainerWithHover = styled.div`
