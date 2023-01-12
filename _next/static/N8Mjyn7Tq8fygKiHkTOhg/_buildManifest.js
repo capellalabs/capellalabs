9b1c48e9688dc4fe4f697c75a6ed2a2b75967750
emadbaqeri <ebaqeri@protonmail.com>
2023-01-12 16:32:01 +0000

    Deploying to gh-pages from @ capellalabs/capellalabs@b2d7c132e8f433f5d909e4a4f5ad452a5eea51bd 🚀

--- _next/static/N8Mjyn7Tq8fygKiHkTOhg/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/406-0b4c20bf54619661.js","static/chunks/pages/index-8402da6481e17c5f.js"],"/_error":["static/chunks/pages/_error-47e526b08b70a03d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/fJgwW3xh5IbCrbgou_6-8/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/406-0b4c20bf54619661.js","static/chunks/pages/index-be8ab46ca3132e74.js"],"/_error":["static/chunks/pages/_error-47e526b08b70a03d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
